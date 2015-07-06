254862e29fe3e8101b937f0da680bdea69aaac07
crazyscouter <crazyscouter1@gmail.com>
2015-07-06 12:58:27 -0700

    Made better.

--- Police Door/lua/autorun/server/pd_init.lua
+++ Police Door/lua/autorun/server/pd_init.lua
@@ -1,47 +1,47 @@
-PD_Door = {}
+local maps = {}
+maps["gm_flatgrass"] = {
+	{Vector(506.866211,-50.352909,-12223.968750), Angle(0,0,0)}
+}
+maps["gm_flatgrass"] = {
+	{Vector(529.653381,-78.400650,-12223.968750), Angle(32,0,0)}
+}
+	 
+maps["rp_downtown_v2"] = { 
+	{Vector(-1572.088867,117,-131.968750), Angle(0,0,0)}, 
+	{Vector(-1455.483032,117,-131.968750), Angle(0,0,0)} 
+}	
 
-PD_Door.AddDoor = function(map,pos,ang)
-	if string.lower(game.GetMap())==string.lower(map) then
-		local Door = ents.Create("pd_door")
-		Door:SetPos(pos)
-		Door:SetPos(Door:LocalToWorld(Vector(0,0,-20)))
-		Door:SetAngles(Angle(0,0,0))
-		Door:Spawn()
-		Door:Activate()
-		
-				Door:SetAngles(Door:LocalToWorldAngles(ang))
-		
-		print("Door added")
-	end
-end
-
-PD_Door.RemoveDoor = function(map,pos)
-	if string.lower(game.GetMap())==string.lower(map) then
-		local Ents = ents.FindInSphere(pos,20)
-		
-		for k,v in pairs(Ents) do
-			if v:GetClass()=="prop_door_rotating" then
-				v:Remove()
-			end
-		end
-	end
-end
-
-PD_Door.SpawnDoors = function()
+hook.Add("InitPostEntity", "PD_Door_Spawn", function()
 	for k,v in pairs(ents.GetAll()) do
 		if v:GetClass()=="pd_door" then
 			v:Remove()
 		end
 	end
 
-	PD_Door.AddDoor("gm_flatgrass",Vector(506.866211,-50.352909,-12223.968750),Angle(0,0,0))
-	PD_Door.AddDoor("gm_flatgrass",Vector(529.653381,-78.400650,-12223.968750),Angle(32,0,0))
+	local map = string.lower(game.GetMap())
+	local pos = maps[map] 
+	if (!pos) then return end
 	
-	PD_Door.AddDoor("rp_downtown_v2",Vector(-1572.088867,117,-131.968750),Angle(0,0,0))
-	PD_Door.AddDoor("rp_downtown_v2",Vector(-1455.483032,117,-131.968750),Angle(0,0,0))
-	
-	PD_Door.RemoveDoor("rp_downtown_v2",Vector(-1572.088867,116.597923,-131.968750))
-	PD_Door.RemoveDoor("rp_downtown_v2",Vector(-1455.483032,113.012917,-131.968750))
-end
+	for key, tbl in pairs(pos) do
+		local Ents = ents.FindInSphere(tbl[1],20)
+		
+		if (#Ents > 0) then
+			for k,v in pairs(Ents) do
+				if v:GetClass() == "prop_door_rotating" then
+					v:Remove()
+				end
+			end
+		end
 
-hook.Add("InitPostEntity","PD_Door_Spawn",PD_Door.SpawnDoors)
+		local Door = ents.Create("pd_door")
+		Door:SetPos(tbl[1])
+		Door:SetPos(Door:LocalToWorld(Vector(0,0,-20)))
+		Door:SetAngles(tbl[2])
+		Door:Spawn()
+		Door:Activate()
+		
+		Door:SetAngles(Door:LocalToWorldAngles(tbl[2]))
+		
+		print("Door added")
+	end
+end)
